25e1971b1acc16f4837656825267572f75c6cfe1
redvg <redvg17@gmail.com>
2018-08-09 11:45:15 +0200

    adds path function

--- monte_carlo_job.py
+++ monte_carlo_job.py
@@ -3,7 +3,22 @@ import time
 from operator import add
 
 INITAL_INVESTMENT = 100000
-ANNUAL_INVESTMENT = 10000
+ANNUAL_INVESTMENT_BUMP = 10000
 T = 30
 RETURN = 0.11
 STD_DEV = 0.18
+
+
+def create_path(seed):
+
+    random.seed(seed)
+
+    portfolio_value = INITAL_INVESTMENT
+
+    for _ in range(T):
+
+        growth = random.normalvariate(RETURN, STD_DEV)
+
+        portfolio_value += portfolio_value * growth + ANNUAL_INVESTMENT
+
+    return portfolio_value
